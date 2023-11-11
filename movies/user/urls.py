74fdbc8ed859a200dd154a612b4511e82a9bec34
ovechkina-diana <64907906+ovechkina-diana@users.noreply.github.com>
2023-11-11 19:01:15 +0300

    Update urls.py

--- movies/user/urls.py
+++ movies/user/urls.py
@@ -2,7 +2,6 @@
 from django.urls import path
 from . import views
 
-# определение маршрутов URL для приложение user
 urlpatterns = [
     path('register/', views.RegisterUser.as_view(), name='register'),
     path('login/', views.LoginUser.as_view(), name='login'),
