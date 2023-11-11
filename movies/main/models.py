20e4622d2585f9c7cbc48b0d112b89d23486ac9b
ovechkina-diana <64907906+ovechkina-diana@users.noreply.github.com>
2023-11-11 19:04:22 +0300

    Update models.py

--- movies/main/models.py
+++ movies/main/models.py
@@ -3,10 +3,7 @@ from slugify import slugify
 from django.urls import reverse
 
 
-# ДАННЫЕ МОЕДИЛИ СОЗДАЮТ ТАБЛИЦЫ НА ОСНОВЕ МОДЕЛЕЙ В БД
 
-# создание модели Actor с полями - name и surname.
-# определяется строковое представление объекта Actor и метаданные модели для отображения у администратора
 class Actor(models.Model):
     name = models.CharField('Имя', max_length=100)
     surname = models.CharField('Фамилия', max_length=150)
@@ -18,8 +15,6 @@ class Actor(models.Model):
         verbose_name = 'Актер'
         verbose_name_plural = 'Актеры'
 
-# создание модели Genre с  полем name.
-# определяется строковое представление объекта Genre и метаданные модели для отображения у администратора
 class Genre(models.Model):
     name = models.CharField('Название', max_length=50)
 
@@ -31,8 +26,6 @@ class Genre(models.Model):
         verbose_name_plural = 'Жанры'
 
 
-# создание модели Director с полями - name и surname.
-# определяется строковое представление объекта Director и метаданные модели для отображения у администратора
 class Director(models.Model):
     name = models.CharField('Имя', max_length=100)
     surname = models.CharField('Фамилия', max_length=150)
@@ -44,8 +37,6 @@ class Director(models.Model):
         verbose_name = 'Режиссер'
         verbose_name_plural = 'Режиссеры'
 
-# создание модели Country с  полем name.
-# определяется строковое представление объекта Country и метаданные модели для отображения у администратора
 class Country(models.Model):
     name = models.CharField('Название', max_length=100)
 
@@ -56,9 +47,7 @@ class Country(models.Model):
         verbose_name = 'Страна'
         verbose_name_plural = 'Страны'
 
-# создание модели Film с полями title, country, year, genre, directors, actors, rating, description, poster, slug
-# определяется строковое представление объекта Country и метаданные модели для отображения у администратора
-# для genre, directors, country, actors автоматически создаются дополнительные таблицы, которые обеспечивают связи многие ко многим
+
 class Film(models.Model):
     title = models.CharField('Название', max_length=150)
     country = models.ManyToManyField(Country)
@@ -71,8 +60,6 @@ class Film(models.Model):
     poster = models.URLField('Постер')
     slug = models.SlugField(max_length=150,unique=True, null=True)
 
-    # Метод save переопределен для модели, чтобы автоматически генерировать slug
-    # на основе title с помощью функции slugify перед сохранением объекта
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title)
         super(Film, self).save(*args, **kwargs)
