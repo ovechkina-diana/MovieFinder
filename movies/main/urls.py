9dc37638457d9c007988357586e031067b356173
ovechkina-diana <64907906+ovechkina-diana@users.noreply.github.com>
2023-11-11 19:04:41 +0300

    Update urls.py

--- movies/main/urls.py
+++ movies/main/urls.py
@@ -2,11 +2,9 @@
 from django.urls import path
 from . import views
 
-# определение маршрутов URL для приложение main
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='home'),
-    # в адресной строке, благодаря слагу отображаются названия конкретных фильмов, в зависимости от страницы
     path('film/<slug:slug>', views.FilmDeatilView.as_view(), name='movie-detail'),
     path('movies-rec', views.movies_rec, name='movies-rec'),
     path('movie-list/', views.movie_list, name='movie-list')
